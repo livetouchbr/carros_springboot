203a70c19a405bf016f2083b1299093cb8824572
rlecheta <rlecheta@livetouch.com.br>
2019-05-03 16:39:10 -0300

    security memory

--- src/main/java/com/carros/security/BasicConfiguration.java
+++ src/main/java/com/carros/security/BasicConfiguration.java
@@ -2,22 +2,18 @@ package com.carros.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
-//https://www.baeldung.com/spring-security-method-security
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class BasicConfiguration extends WebSecurityConfigurerAdapter {
 
@@ -31,23 +27,6 @@ public class BasicConfiguration extends WebSecurityConfigurerAdapter {
                 .httpBasic()
                 .and().csrf().disable();
     }
-
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth)
-//            throws Exception {
-////        auth
-////                .inMemoryAuthentication()
-////                .passwordEncoder(new BCryptPasswordEncoder())
-////                .withUser("user").password("$2a$10$j4cTpMvrvbZi3MggidCuoeFl92Ql95KWPJRmO8zcHdC6GDzBYQJ92").roles("USER")
-////                .and()
-////                .withUser("admin").password("$2a$10$AxQ/Gl22aSsFaqCVouwWheubK2NuZxPEoNN/2RP7JP8dhst4sbRdu").roles("USER", "ADMIN");
-//
-//        auth
-//                .inMemoryAuthentication()
-//                .withUser("user").password("user").roles("USER")
-//                .and()
-//                .withUser("admin").password("admin").roles("ADMIN");
-//    }
 
     @Bean
     public UserDetailsService userDetailsService() {
